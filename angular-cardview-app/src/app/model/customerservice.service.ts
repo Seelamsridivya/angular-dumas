2d5168976e294e57019b993cd252d61638582ce8
SEELAM SRI DIVYA <31986435+Seelamsridivya@users.noreply.github.com>
2018-10-25 16:32:20 -0400

    i have changed index to original

--- angular-cardview-app/src/app/model/customerservice.service.ts
+++ angular-cardview-app/src/app/model/customerservice.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Icustomer } from './icustomer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerserviceService {
+  url: string='http://localhost:3000/customers'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getCustomers():Observable<Icustomer[]>{
+    return this.http.get<Icustomer[]>(this.url);
+    
+  }
 }
+
+
+
